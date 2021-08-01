a6cdc091800442478834f2ba05704f6f8d4bfc06
Gwyneid <71380716+Gwyneid@users.noreply.github.com>
2021-08-01 17:06:25 +1000

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -16,7 +16,8 @@
  }
  
 let HumanEnemy = new enemy("HumanEnemy", 100, 1, [15, 0, 45], [], [], 50, "./assets/gifs/enemy.gif"); // временнаф гифка
-let ArcherEnemy = new enemy("ArcherEnemy", 120, 1, [30, 30, 30], [], [], 50, "./assets/gifs/enemy.gif"); // временная гифка
-
+let ArcherEnemy = new enemy("ArcherEnemy", 120, 1, [30, 30, 30], [], [], 55, "./assets/gifs/archerenemy.gif"); // временная гифка
+let SmallEnemy = new enemy("SmallEnemy", 80, 1, [25, 30, 20], [], [], 40, "./assets/gifs/smallenemy.gif");
+let TankEnemy = new enemy("TankEnemy", 250, 1, [30, 35, -20], [], [], 70, "./assets/gifs/tankenemy.gif");
 //массив злодеев
-let enemiesArray = [HumanEnemy, ArcherEnemy];
+let enemiesArray = [HumanEnemy, ArcherEnemy, SmallEnemy, TankEnemy];
